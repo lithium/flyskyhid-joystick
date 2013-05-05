dfab57df029bd05225dd16e429ae3e295fef76af
lithium <code@hlidskialf.com>
2013-05-04 18:50:11 -0700

    fly sky joy app

---
+++ FlySkyJoy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlySkyJoy
+//
+//  Created by Wiggins on 5/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FlySkyJoy/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  FlySkyJoy
+//
+//  Created by Wiggins on 5/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
